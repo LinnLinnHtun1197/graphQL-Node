6b6fcd3d07462f1aa7376d361ef00f78c66bf44a
linlintun1197@gmail.com <linlintun1197@gmail.com>
2024-02-12 10:51:44 +0800

    addednewproduct

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
         id: 'redshoe',
         description: 'Red shoe',
         price: 42.12,
+        reviews: [],
     },
     {
         id: 'bluejean',
         description: 'Blue Jeans',
-        price: 55.55
+        price: 55.55,
+        reviews: []
     }
 ];
 
@@ -15,6 +17,32 @@ function getAllProducts() {
     return products;
 }
 
+function getProductsByPrice(min, max) {
+    return products.filter((product) => {
+        return product.price >= min && product.price <= max;
+    })
+}
+
+function getProductById(id) {
+    return products.find((product) => {
+        return product.id === id;
+    })
+}
+
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: []
+    };
+    products.push(newProduct);
+    return newProduct;
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductsByPrice,
+    getProductById,
+    addNewProduct
 }
